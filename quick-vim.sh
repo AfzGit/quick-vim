3a92c00a6f64397f77901d8be3e2d48b1f977e2a
AfzGit <you@example.com>
2023-04-18 17:23:09 +0530

    vimplug fixes

--- quick-vim.sh
+++ quick-vim.sh
@@ -8,13 +8,7 @@ curl -s "$configurl" >> /home/$USER/.vimrc
 echo "Config file installed"
 
 # Installing VIM-Plug
-read -p "Install VIM-Plug? (Plugin installer) [Y/n]" choice
-if [[ "$choice" = "n" || "$choice" = "N" ]]; then
-    echo "Exiting..."
-    exit
-else
-    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    echo "Add plugins in ~/.vimrc file and then run :PlugInstall"
-fi
-
+echo "installing VIM Plug"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "Add plugins in ~/.vimrc file and then run :PlugInstall"
 echo "Done."
